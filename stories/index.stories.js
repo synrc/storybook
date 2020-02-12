f1302116e0ea48016f47b1767523b2e12bceb519
SunRiseGG <SunRiseGC4@gmail.com>
2020-02-12 15:44:41 +0200

    Stories added

---
+++ stories/index.stories.js
@@ -0,0 +1,15 @@
+import comboLookUp from '../public/combolookup.html';
+import comboLookUp2 from '../public/combolookup2.html';
+import calendar from '../public/calendar.html';
+
+export default {
+  title: 'Controls',
+};
+
+export const ComboLookUp2 = () => comboLookUp2;
+
+export const ComboLookUp  = () => {
+  return comboLookUp;
+};
+
+export const Calendar = () => calendar;
